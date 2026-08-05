2066126bd786e4b4d89c19b4aab6365cfff62e78
258i019i-coder <258i019i@gsuite.kobe-u.ac.jp>
2026-08-05 16:00:03 +0000

    pytestの再実行

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,9 @@
 from main import main
 import pandas as pd
+from pathlib import Path
 
 
-def test_main_output(monkeypatch):
+def test_main_output(monkeypatch, tmp_path):
 
     # visualization.py内のinput()に自動で値を渡す
     monkeypatch.setattr(
@@ -20,3 +21,11 @@ def test_main_output(monkeypatch):
 
     # 必要な列が存在することを確認
     assert "community_area" in chicago.columns
+
+    # jpgファイルが作成されたことを確認
+    jpg_file = Path("chicago_trend.jpg")
+
+    assert jpg_file.exists()
+
+    # jpgファイルが空でないことを確認
+    assert jpg_file.stat().st_size > 0
