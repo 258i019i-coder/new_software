82f3a4440bfcabb320eb97e0e8a19f861954b86a
258i019i-coder <258i019i@gsuite.kobe-u.ac.jp>
2026-08-05 09:56:24 +0000

    merge.pyの修正1

--- merge.py
+++ merge.py
@@ -1,41 +1,30 @@
 import pandas as pd
 
 
-def create_chicago():
-
-    from crime import create_crime_summary
-    from acs import create_acs_summary
-
-
-    crime = create_crime_summary()
-
-    acs = create_acs_summary()
+def create_chicago(crime_summary, acs_summary):
 
     header = pd.read_csv(
         "header.csv"
     )
 
-
     chicago = pd.merge(
         header,
-        crime,
+        crime_summary,
         on=[
             "year",
             "community_area"
         ],
         how="left"
     )
-
 
     chicago = pd.merge(
         chicago,
-        acs,
+        acs_summary,
         on=[
             "year",
             "community_area"
         ],
         how="left"
     )
-
 
     return chicago
